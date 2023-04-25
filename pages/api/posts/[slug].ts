3531481423b24240c5b94f6257437a45af66a8a1
qdimanche <73292719+qdimanche@users.noreply.github.com>
2023-04-25 16:34:49 +0200

    [+] update api

---
+++ pages/api/posts/[slug].ts
@@ -0,0 +1,23 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import {db} from "@/lib/db";
+
+export default async function signin(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+
+    const {slug} = req.query
+
+    const post = await db.post.findFirst({
+        where: {
+            slug: slug?.toString(),
+        },
+    });
+
+    if (post) {
+        res.json({data: post})
+    } else {
+        res.status(401);
+        res.json({error: "Invalid postId"});
+    }
+}
