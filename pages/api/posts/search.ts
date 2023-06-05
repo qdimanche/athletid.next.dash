7958e92dac083e34009163bf4936f9011831e013
qdimanche <73292719+qdimanche@users.noreply.github.com>
2023-06-05 12:10:55 +0200

    [+] add search post api

--- pages/api/posts/search.ts
+++ pages/api/posts/search.ts
@@ -5,6 +5,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     if (req.method === "GET") {
         try {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+            res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+
             const {q: query} = req.query;
 
             if (typeof query !== "string") {
