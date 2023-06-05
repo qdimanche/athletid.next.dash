ab96608e0985841ccf490b188857205800a67a4e
qdimanche <73292719+qdimanche@users.noreply.github.com>
2023-06-05 14:04:05 +0200

    [+] add search post api

--- pages/api/posts/search.ts
+++ pages/api/posts/search.ts
@@ -1,17 +1,28 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import {db} from "@/lib/db";
 import NextCors from 'nextjs-cors';
+import Cors from 'cors'
 
+const cors = Cors({
+    methods: ['GET', 'HEAD'],
+})
+
+
+function runMiddleware(req:NextApiRequest, res:NextApiResponse, fn:any) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result:any) => {
+            if (result instanceof Error) {
+                return reject(result)
+            }
+
+            return resolve(result)
+        })
+    })
+}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
-
-    await NextCors(req, res, {
-        // Options
-        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-        origin: '*',
-        optionsSuccessStatus: 200,
-    });
+    await runMiddleware(req, res, cors);
 
     if (req.method === "GET") {
         try {
